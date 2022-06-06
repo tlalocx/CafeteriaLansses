41d220bf0e5335e28f30ad77ccfbbeaff63a8741
tlalocx <emiliochavez.dev@gmail.com>
2022-06-06 12:42:06 -0600

    Normal upkeep

---
+++ sql/data_productos.sql
@@ -0,0 +1,90 @@
+INSERT INTO
+    `productos` (
+        `id_producto`,
+        `nombre_producto`,
+        `categoria_producto`,
+        `descripcion_producto`,
+        `imagen_producto`,
+        `precio_producto`
+    )
+VALUES
+    (
+        NULL,
+        'Caramel Mocha',
+        'Bebidas',
+        'Preparado con café espresso. Además, contiene leche entera o sin grasa al vapor, sirope de chocolate y caramelo, y se cubre con un topping de crema batida y un chorrito de caramelo.',
+        'caramel-mocha.jpg',
+        '45'
+    ),
+    (
+        NULL,
+        'Matcha Latte',
+        'Bebidas',
+        'Hecho con té verde en polvo y leche al vapor.',
+        'matcha-latte.jpg',
+        '40'
+    ),
+    (
+        NULL,
+        'Latte',
+        'Bebidas',
+        'De origen italiano, elaborado con espresso y leche vaporizada.',
+        'latte.jpg',
+        '40'
+    ),
+    (
+        NULL,
+        'Espresso',
+        'Bebidas',
+        'De origen italiano, se fuerza una pequeña cantidad de agua casi hirviendo bajo 9-10 bares de presión a través de granos de café finamente molidos.',
+        'espresso.jpg',
+        '35'
+    ),
+    (
+        NULL,
+        'Tiramisu',
+        'Postres',
+        'Hecho de melindres sumergidos en café, recubiertos con una mezcla batida de huevos, azúcar y queso mascarpone, aromatizados con cacao.',
+        'tiramisu.jpg',
+        '50'
+    ),
+    (
+        NULL,
+        'Cheesecake',
+        'Postres',
+        'Mezcla de queso fresco suave, huevos y azúcar. Con una base hecha de galletas trituradas.',
+        'cheesecake.jpg',
+        '50'
+    ),
+    (
+        NULL,
+        'BLT',
+        'Sandwiches',
+        'Sándwich, llamado así por las iniciales de sus ingredientes principales, tocino, lechuga y tomate.',
+        'blt.jpg',
+        '55'
+    ),
+    (
+        NULL,
+        'Vegetariano',
+        'Sandwiches',
+        'Mezcla selecta de vegetales entre pan artesanal de casa.',
+        'vegetariano.jpg',
+        '45'
+    ),
+    (
+        NULL,
+        'Belgian',
+        'Waffles',
+        'Masa liviana, cuadrados grandes y profundos.',
+        'belgian.jpg',
+        '45'
+    ),
+    (
+        NULL,
+        'Waffle Bites',
+        'Waffles',
+        'Waffles clasicos en tamaño reducido',
+        'waffle-bites.jpg',
+        '45'
+    );
